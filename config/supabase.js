cbe3f30407b2e7c45472dcd64ed97a440985ded3
AbelClapton <abelrguezxd@gmail.com>
2023-05-14 17:44:27 -0400

    add supabase config

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,7 @@
 export default {
   credentials: {
-    supabaseUrl: 'https://xxxxxxxxxxxxxxxxxxxx.supabase.co',
-    supabaseKey: 'XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXx.XXXXXXXXXXXXX'
-  }
-}
+    supabaseUrl: 'https://qxidrtnbgmkyzvdfplln.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4aWRydG5iZ21reXp2ZGZwbGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5Njk4NTMsImV4cCI6MTk5ODU0NTg1M30.Hg6q5khnKxXFWPRqA3YI9he3hEajpBbbJRHybaiDe8Q',
+  },
+};
